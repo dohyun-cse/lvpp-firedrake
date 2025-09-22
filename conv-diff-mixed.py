7de2d4b0b15e6857326b731bdb446db584238796
Dohyun Kim <dohyun_kim@brown.edu>
2025-09-22 11:20:17 -0400

    time dependent

--- conv-diff-mixed.py
+++ conv-diff-mixed.py
@@ -3,10 +3,10 @@ from firedrake import *
 import matplotlib.pyplot as plt
 plt.ion() # Turn on interactive mode
 
-alpha_max = 1e05
+comm = COMM_WORLD
 
 # Define the domain
-mesh = PeriodicRectangleMesh(128, 128, 1.0, 1.0)
+mesh = PeriodicRectangleMesh(128, 128, 1.0, 1.0, comm=comm)
 dt = 1.0 / 128 / 5
 T = 1.0
 # Define function spaces
@@ -17,9 +17,10 @@ W = VectorFunctionSpace(mesh, "CG", 2, dim=2) # For velocity field
 
 # Define the right-hand side
 x, y = SpatialCoordinate(mesh)
-b = Function(W).interpolate(5*as_vector((y - 0.5, -x + 0.5))) # Velocity field
-K = Constant(0.01) # Diffusion coefficient
-u_init = Function(primal_space).interpolate(exp(-50*((x - 0.25)**2 + (y - 0.5)**2)))
+b = Function(W).interpolate(as_vector((1.0, 0.0))) # Velocity field
+K = Constant(1e-03) # Diffusion coefficient
+# K = Constant(0.0) # Diffusion coefficient
+u_init = Function(primal_space).interpolate(ufl.conditional(ufl.And(ufl.le(abs(x-0.5), 0.125-1e-09), ufl.le(abs(y-0.5), 0.125-1e-09)), 1.0, 0.0))
 
 # Solution (Symbolic + Discrete) 
 sol = Function(V)
@@ -40,7 +41,9 @@ u_k, psi_k = sol_k.subfunctions
 mapped_uh = Function(latent_space)
 
 # Mirror map (\nabla R^*)
-mirror_map = exp(psi)
+mirror_map = ufl.conditional(ufl.ge(psi, 0.0),
+                             1.0 / (1.0 + ufl.exp(-psi)),
+                             ufl.exp(psi) / (1.0 + ufl.exp(psi)))
 
 # Define the saddle-point Lagrangian
 v, phi = TestFunctions(V)
@@ -50,31 +53,24 @@ prox_diff = (psi - psi_k)*v
 consistency = (u - mirror_map)*phi
 F = (alpha*A + prox_diff + consistency)*dx
 
-# Plotting setup
-fig_primal = plt.figure()
-fig_dual = plt.figure()
-def plot_solution(fig, uh):
-    fig.clear()
-    axes = fig.add_subplot(111)
-    colors = tricontourf(uh, axes=axes)
-    fig.colorbar(colors, ax=axes)
-    fig.canvas.draw()
-    fig.canvas.flush_events()
+outfile = VTKFile("conv-diff/solution.pvd", project_output=True, comm=comm)
 
 t = 0.0
 alpha.assign(1.0)
 # Map the dual variable to the primal space for plotting
-mapped_uh.interpolate(mirror_map)
-plot_solution(fig_primal, uh)
-plot_solution(fig_dual, mapped_uh)
+outfile.write(uh, mapped_uh, time=t)
 while t < T:
     t += dt
     sol_prev.assign(sol)
-    print(f'Time = {t:.3f}')
+    psih.assign(0.0)
+    alpha.assign(min(1e04, alpha.values()[0]))
+    if comm.rank == 0:
+        print(f'Time = {t:.3f}')
     for i in range(1, 101):
         sol_k.assign(sol)
-        for attempt in range(5):
-            print(f'\tIteration {i:3d}, alpha = {alpha.values()[0]:.2e}', end='')
+        for attempt in range(10):
+            if comm.rank == 0:
+                print(f'\tIteration {i:3d}, alpha = {alpha.values()[0]:.2e}', end='')
             try:
                 solve(F == 0, sol,
                       solver_parameters={"snes_rtol": 0.0, "snes_atol": 1e-8, "ksp_type": "gmres"})
@@ -82,15 +78,18 @@ while t < T:
             except ConvergenceError:
                 sol.assign(sol_k) # Reset to previous iterate
                 alpha.assign(0.5*alpha) # Reduce step size
-                print(' ... failed to converge, reducing alpha and retrying')
+                if comm.rank == 0:
+                    print(' ... failed to converge, reducing alpha and retrying')
                 continue
+        else:
+            raise ConvergenceError('Nonlinear solver failed to converge')
 
 
         primal_succ_norm = norm(u - u_k)
-        print(f': primal success norm = {primal_succ_norm:.4e}')
+        if comm.rank == 0:
+            print(f': primal success norm = {primal_succ_norm:.4e}')
         if (primal_succ_norm < 1e-07):
             break
-        alpha.assign(min(2.0*alpha.values()[0], alpha_max))
+        alpha.assign(2.0 * alpha)
     mapped_uh.interpolate(mirror_map)
-    plot_solution(fig_primal, uh)
-    plot_solution(fig_dual, mapped_uh)
+    outfile.write(uh, mapped_uh, time=t)
